61ea5032bff5a8d83acf714fb83da09904ce9fa0
Lucas Mota <lucasmota7@gmail.com>
2021-11-27 13:06:34 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe a distancia percorrida: ");
+            double distancia = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe o tempo gasto para percorrer " + distancia + " Km: ");
+            double tempo = double.Parse(Console.ReadLine());
+
+            double velocidadeMedia = distancia / tempo;
+
+            Console.Write("A velocidade média do percurso foi de: " + velocidadeMedia + " Km/h.");
+
+            Console.ReadKey();
+        }
+    }
+}
